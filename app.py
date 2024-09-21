921ddf3fa5ecfe441b6f141c23f1a3058ab7ec39
jeevake <jeevakeperera@gmail.com>
2024-09-21 18:10:26 +0530

    first commit

--- app.py
+++ app.py
@@ -238,15 +238,12 @@ def predict():
         image = Image.open(file.stream)
         print(f' * Image: {image}')
 
-        img_path = "C:/Users/Jeevake/Seacucumber/2024_09_15/demarcations/Bohadschia Marmorata Class A/PXL_20240408_051936593.jpg"
+        #img_path = "C:/Users/Jeevake/Seacucumber/2024_09_15/demarcations/Bohadschia Marmorata Class A/PXL_20240408_051936593.jpg"
         class_labels = ['Bohadschia Marmorata Class A', 'Bohadschia Vitiensis Class A', 'Holothuria Spinifera Class A', 'Holothuria Spinifera Class B', 'Stichopus Naso Class A','Holothuria Scabra Class C','Holothuria Scabra Class A','Holothuria Scabra Class B']
 
         model,yolo_model = load_models()
         pred_label,pred_prob,text = prediction_probability_label(model,yolo_model, image, class_labels)
 
-        # print(f'Predicted label for the image: {pred_label}')
-        # print(f'Confidence level: {pred_prob}')
-        # print(f'Text: {text}')
         
         response = {
             'prediction': {
